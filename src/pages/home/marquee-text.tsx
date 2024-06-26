0d0606b1b0597cbe0bc118fa4a65b9be365d226a
Kopalmuhamad <muhamadkopal32@gmail.com>
2024-06-26 12:09:50 +0700

    adding time in marquee text

--- src/pages/home/marquee-text.tsx
+++ src/pages/home/marquee-text.tsx
@@ -37,7 +37,7 @@ const MarqueeText: React.FC<ParallaxProps> = ({
   const x = useTransform(baseX, (v) => `${wrap(-20, -45, v)}%`);
 
   const directionFactor = useRef<number>(1);
-  useAnimationFrame((delta) => {
+  useAnimationFrame((_, delta) => {
     let moveBy = directionFactor.current * baseVelocity * (delta / 1000);
 
     if (velocityFactor.get() < 0) {
