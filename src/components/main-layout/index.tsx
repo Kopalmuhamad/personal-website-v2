2afd61b8407c641fd8f5a32f2b0c5aa84b4fe86c
Kopalmuhamad <muhamadkopal32@gmail.com>
2024-06-26 12:03:47 +0700

    fixing code for deploymen

--- src/components/main-layout/index.tsx
+++ src/components/main-layout/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "../navbar";
 import Footer from "../footer";
-import AnimatedBackground from "../animated-background/animated-backgrouond";
 
 interface IMainLayoutProps {
   children: React.ReactNode;
